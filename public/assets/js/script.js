7c95ec8bdef2a372ab5937e2e292d58ed1e5ae62
nainxav <nainciaa@gmail.com>
2026-01-28 12:05:52 +0700

    Edit ReadMe

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -343,8 +343,8 @@ if (Number(drone.human_detected) === 1) {
     const date = new Date(timestamp);
     const MM = String(date.getMonth() + 1).padStart(2, "0");
     const DD = String(date.getDate()).padStart(2, "0");
-    const HH = String(date.getHours()).padStart(2, "0");
-    const mm = String(date.getMinutes()).padStart(2, "0");
+    const HH = String(date.getUTCHours()).padStart(2, "0");
+    const mm = String(date.getUTCMinutes()).padStart(2, "0");
     const ss = String(date.getSeconds()).padStart(2, "0");
 
     const filename = `${MM}-${DD}-${HH}-${mm}-${ss}.jpg`;
